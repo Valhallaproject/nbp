f5b6b7abe01d4eb9e77f101dd20521d809156a61
Nicolas Bictel <valhallaproject@outlook.fr>
2022-10-20 18:15:04 +0200

    add contact

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Banner.css"
 
 function banner() {
   return (
     <div className="banner">
-      <h1>N I K O L A B I K T E L</h1>
+      <h1><Link to="/">N I K O L A B I K T E L</Link></h1>
     </div>
   )
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Insta from "./../../assets/instagram.png"
 import "./Contact.css"
 
 function Contact() {
   return (
-    <div className="contact"></div>
+    <div className="contact">
+      <h2>CONTACT</h2>
+      <form>
+        <label>
+          <input type="text" placeholder="Name"/>
+        </label>
+        <label>
+          <input type="email" placeholder="E-mail"/>
+        </label>
+        <label>
+          <textarea type="text" placeholder="Message"/>
+        </label>
+        <button type="submit"className="button">Send</button>
+      </form>
+      <h2>SOCIAL MEDIA</h2>
+      <img src={Insta} alt="instagramLogo" className="insta"/>
+      <p>© NIKOLA BIKTEL. All rights reserved.</p>
+    </div>
   )
 }
 
